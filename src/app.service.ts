1f56e78cb0cd71d563cc4e28b416cce2cb14e20f
Maksim Shuklin <makshuk@bk.ru>
2024-02-25 19:30:54 +0300

    Добавлен импорт и использование библиотеки node-global-key-listener для обработки глобальных клавиш

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { RecordService } from './record/record.service';
 import { OpenaiService } from './openai/openai.service';
+import { GlobalKeyboardListener } from 'node-global-key-listener';
 import * as copyPaste from 'copy-paste';
 import * as fs from 'fs';
 
@@ -10,15 +11,24 @@ export class AppService {
 		private readonly recordService: RecordService,
 		private readonly openaiService: OpenaiService,
 	) {}
-	async recordAndConvertToText(): Promise<string> {
+	recordAndConvertToText = async (): Promise<string> => {
 		await this.recordService.recordVoice();
 		await this.recordService.convertToWav();
 		const stream = fs.createReadStream('C:/development/NestJS/speak-to-text/output.wav');
 		const result = await this.openaiService.transcriptionAudio(stream);
 		const text = result.content || 'Ошибка при чтении файла';
 		copyPaste.copy(text);
+		copyPaste.paste();
 		return text;
-	}
+	};
 
-	keyAction(): void {}
+	keyAction(): void {
+		const v = new GlobalKeyboardListener();
+
+		v.addListener(e => {
+			if (e.state == 'DOWN' && e.name == 'F19') {
+				this.recordAndConvertToText();
+			}
+		});
+	}
 }
